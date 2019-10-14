f13e81f1d4a37cbe3df395759d788e9cdb891574
Benny Joseph <jbenny37@gmail.com>
2019-10-14 13:04:23 +0530

    MergeSort

--- DataStructuresLab/MergeSort.c
+++ DataStructuresLab/MergeSort.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void merge(int *, int , int , int );
-void mergeSort(int *, int , int );
+void merge(int *, int, int, int);
+void mergeSort(int *, int, int);
 void displayArray(int *, int);
 
 int main()
@@ -20,7 +20,7 @@ int main()
     printf("Elements before sorting\n");
     displayArray(arr, number_of_ele);
 
-    mergeSort(arr, 0, number_of_ele);
+    mergeSort(arr, 0, number_of_ele - 1);
 
     printf("Elements after sorting\n");
     displayArray(arr, number_of_ele);
@@ -37,55 +37,45 @@ void displayArray(int *arr, int n)
     printf("\n");
 }
 
-void mergeSort(int *a, int l, int h)
+void mergeSort(int *arr, int left, int right)
 {
-    /*The function mergeSort takes 3 parameters
-    a[]- The array to be sorted
-    l-Starting index of the list
-    h-Ending index of the list*/
-    if (l < h)
+
+    if (left < right)
     {
-        int mid = (l + h) / 2;
-        mergeSort(a, l, mid);
-        mergeSort(a, mid + 1, h);
-        merge(a, l, mid, h);
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
-void merge(int *a, int l, int m, int h)
+void merge(int *arr, int left, int middle, int right)
 {
-    /*
-    The function merge take 4 parameters 
-    a-integr array 
-    l- is the starting of list 1
-    m- is the ending index of list1
-        and j+1 is the satring index of list 2
-    h- is the ending of list 2
-    and the return void
-*/
-    int b[100];
-    int i = l, j = m + 1, k = 0;
-    while (i <= m && j <= h)
+    int auxilliary[20];
+    int i = left, j = middle + 1, k = 0;
+    while (i <= middle && j <= right)
     {
-        if (a[i] < a[j])
+        if (arr[i] < arr[j])
         {
-            b[k++] = a[i++];
+            auxilliary[k++] = arr[i++];
         }
         else
         {
-            b[k++] = a[j++];
+            auxilliary[k++] = arr[j++];
         }
     }
-    while (j <= h)
+
+    while (j <= right)
     {
-        b[k++] = a[j++];
+        auxilliary[k++] = arr[j++];
     }
-    while (i <= m)
+    while (i <= middle)
     {
-        b[k++] = a[i++];
+        auxilliary[k++] = arr[i++];
     }
-    for (i = l, j = 0; i <= h; i++, j++)
+
+    for (i = left, j = 0; i <= right; i++, j++)
     {
-        a[i] = b[j];
+        arr[i] = auxilliary[j];
     }
 }
